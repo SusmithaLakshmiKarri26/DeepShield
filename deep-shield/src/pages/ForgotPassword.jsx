a6c77a79531503ba85faf73d1601d3fe8018ac27
SusmithaLakshmiKarri26 <147433621+SusmithaLakshmiKarri26@users.noreply.github.com>
2026-03-08 15:33:52 +0530

    Fixed ESLint errors

--- deep-shield/src/pages/ForgotPassword.jsx
+++ deep-shield/src/pages/ForgotPassword.jsx
@@ -14,9 +14,9 @@ const ForgotPassword = () => {
 
     try {
       const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000";
-      const response = await axios.post(
-        `${API_URL}/api/auth/forgot-password`,
-        { email }
+      await axios.post(
+      `${API_URL}/api/auth/forgot-password`,
+      { email }
       );
 
       setMessage("Reset link sent to your email.");
