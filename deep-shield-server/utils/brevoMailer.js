61cd4c0f2990e4e500f53edd24c67af9cd5cf64d
SusmithaLakshmiKarri26 <147433621+SusmithaLakshmiKarri26@users.noreply.github.com>
2026-03-09 10:27:17 +0530

    Updated environmental variables

--- deep-shield-server/utils/brevoMailer.js
+++ deep-shield-server/utils/brevoMailer.js
@@ -16,7 +16,7 @@ const tranEmailApi = new SibApiV3Sdk.TransactionalEmailsApi();
 const sendEmail = async (toEmail, subject, htmlContent) => {
   try {
     await tranEmailApi.sendTransacEmail({
-      sender: { email: "22b01a0579@svecw.edu.in", name: "DeepShield" },
+      sender: { email: "supportfromefv@gmail.com", name: "DeepShield" },
       to: [{ email: toEmail }],
       subject,
       htmlContent
